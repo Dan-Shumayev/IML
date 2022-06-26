Implement comparing of fixed learning rates and Gradient Descent's fit method. TODO - test it!

IMLearn/desent_methods/gradient_descent.py
@@ -83,7 +83,8 @@ class GradientDescent:
 
     def fit(self, f: BaseModule, X: np.ndarray, y: np.ndarray):
         """
-        Optimize module using Gradient Descent iterations over given input samples and responses
+        Optimize module using Gradient Descent iterations over given 
+        input samples and responses
 
         Parameters
         ----------
@@ -96,26 +97,29 @@ class GradientDescent:
 
         Returns
         -------
-        solution: ndarray of shape (n_features)
-            Obtained solution for module optimization, according to the specified self.out_type_
+        solution: ndarray of shape (n_features) 
+            Obtained solution for module optimization, 
+            according to the specified self.out_type_
 
         Notes
         -----
         - Optimization is performed as long as self.max_iter_ has not been reached and that
         Euclidean norm of w^(t)-w^(t-1) is more than the specified self.tol_
 
-        - At each iteration the learning rate is specified according to self.learning_rate_.lr_step
+        - At each iteration the learning rate is specified according to 
+            self.learning_rate_.lr_step
 
-        - At the end of each iteration the self.callback_ function is called passing self and the
-        following named arguments:
+        - At the end of each iteration the self.callback_ function is called 
+            passing self and the following named arguments:
             - solver: GradientDescent
                 self, the current instance of GradientDescent
             - weights: ndarray of shape specified by module's weights
                 Current weights of objective
             - val: ndarray of shape specified by module's compute_output function
                 Value of objective function at current point, over given data X, y
-            - grad:  ndarray of shape specified by module's compute_jacobian function
-                Module's jacobian with respect to the weights and at current point, over given data X,y
+            - grad: ndarray of shape specified by module's compute_jacobian function
+                Module's jacobian with respect to the weights and at current point, 
+                over given data (X,y)
             - t: int
                 Current GD iteration
             - eta: float
@@ -124,4 +128,47 @@ class GradientDescent:
                 Euclidean norm of w^(t)-w^(t-1)
 
         """
-        raise NotImplementedError()
+        best_arg = f.weights
+        min_loss = f.compute_output(X=X, y=y)
+
+        recorded_path = [f.weights]
+
+        def compute_curr_step(eta: float) -> float:
+            f.weights = recorded_path[iteration_num]
+            direction = - f.compute_jacobian(X=X, y=y)
+
+            return recorded_path[iteration_num] + eta * direction
+        
+        def compute_curr_loss_and_arg(curr_loss):
+            f.weights = w_t
+            recorded_path.append(f.weights)
+
+            return curr_loss, recorded_path[iteration_num + 1] \
+                if curr_loss < min_loss else min_loss, best_arg
+
+        for iteration_num in range(self.max_iter_):
+            eta = self.learning_rate_.lr_step(t=iteration_num)
+            w_t = compute_curr_step(eta)
+
+            curr_step = np.linalg.norm(recorded_path[iteration_num + 1] - \
+                recorded_path[iteration_num])
+            curr_loss = f.compute_output(X=X, y=y)
+            min_loss, best_arg = compute_curr_loss_and_arg(curr_loss)
+
+            self.callback_(solver=self,
+                           weights=f.weights,
+                           val=curr_loss,
+                           grad=f.compute_jacobian(X=X, y=y),
+                           t=iteration_num,
+                           eta=eta,
+                           delta=curr_step)
+
+            if curr_step < self.tol_: 
+                break
+
+        if self.out_type_ == "average":
+            return np.mean(np.array(recorded_path))
+        if self.out_type_ == "last":
+            return recorded_path[-1]
+        if self.out_type_ == "best":
+            return best_arg

exercises/gradient_descent_investigation.py
@@ -1,14 +1,14 @@
+from typing import Callable, List, Tuple, Type
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from typing import Tuple, List, Callable, Type
-
+import plotly.graph_objects as go
 from IMLearn import BaseModule
-from IMLearn.desent_methods import GradientDescent, FixedLR, ExponentialLR
+from IMLearn.desent_methods import ExponentialLR, FixedLR, GradientDescent
 from IMLearn.desent_methods.modules import L1, L2
 from IMLearn.learners.classifiers.logistic_regression import LogisticRegression
 from IMLearn.utils import split_train_test
-
-import plotly.graph_objects as go
 
 
 def plot_descent_path(module: Type[BaseModule],
@@ -39,7 +39,8 @@ def plot_descent_path(module: Type[BaseModule],
     Return:
     -------
     fig: go.Figure
-        Plotly figure showing module's value in a grid of [xrange]x[yrange] over which regularization path is shown
+        Plotly figure showing module's value in a grid of [xrange]x[yrange] 
+        over which regularization path is shown
 
     Example:
     --------
@@ -50,22 +51,25 @@ def plot_descent_path(module: Type[BaseModule],
         return np.array([module(weights=wi).compute_output() for wi in w])
 
     from utils import decision_surface
-    return go.Figure([decision_surface(predict_, xrange=xrange, yrange=yrange, density=70, showscale=False),
-                      go.Scatter(x=descent_path[:, 0], y=descent_path[:, 1], mode="markers+lines", marker_color="black")],
-                     layout=go.Layout(xaxis=dict(range=xrange),
-                                      yaxis=dict(range=yrange),
+    return go.Figure([decision_surface(predict_, xrange=xrange, yrange=yrange, \
+                    density=70, showscale=False), \
+                      go.Scatter(x=descent_path[:, 0], y=descent_path[:, 1], \
+                        mode="markers+lines", marker_color="black")], \
+                     layout=go.Layout(xaxis=dict(range=xrange), yaxis=dict(range=yrange),
                                       title=f"GD Descent Path {title}"))
 
 
-def get_gd_state_recorder_callback() -> Tuple[Callable[[], None], List[np.ndarray], List[np.ndarray]]:
+def get_gd_state_recorder_callback() -> Tuple[Callable[[], None], List[np.ndarray], \
+                        List[np.ndarray]]:
     """
-    Callback generator for the GradientDescent class, recording the objective's value and parameters at each iteration
+    Callback generator for the GradientDescent class, 
+    recording the objective's value and parameters at each iteration
 
     Return:
     -------
     callback: Callable[[], None]
-        Callback function to be passed to the GradientDescent class, recoding the objective's value and parameters
-        at each iteration of the algorithm
+        Callback function to be passed to the GradientDescent class, 
+        recording the objective's value and parameters at each iteration of the algorithm
 
     values: List[np.ndarray]
         Recorded objective values
@@ -73,18 +77,58 @@ def get_gd_state_recorder_callback() -> Tuple[Callable[[], None], List[np.ndarra
     weights: List[np.ndarray]
         Recorded parameters
     """
-    raise NotImplementedError()
+    weights, losses = [], []
 
+    def callback(solver=None, weights=None, val=None, grad=None, t=None, eta=None, delta=None):
+        weights.append(weights)
+        losses.append(val)
+
+    return callback, weights, losses
 
 def compare_fixed_learning_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
                                  etas: Tuple[float] = (1, .1, .01, .001)):
-    raise NotImplementedError()
+    out_type = "best"
+
+    for eta in etas:
+        curr_eta = FixedLR(eta)
+        l1_callback, l1_weights, l1_losses = get_gd_state_recorder_callback()
+        l2_callback, l2_weights, l2_losses = get_gd_state_recorder_callback()
+
+        # Initialize L1-2 norms
+        l1_model = L1(init)
+        l2_model = L2(init)
+
+        # Set gradient descent algorithms for both norms with `curr_eta` learning-rate
+        l1_solver, l2_solver = GradientDescent(learning_rate=curr_eta, out_type=out_type, \
+            callback=l1_callback), GradientDescent(learning_rate=curr_eta, \
+                                                out_type=out_type, callback=l2_callback)
+        # Fit these norms by gradient descent iterations (done by `fit`)
+        lowest_loss_l1, lowest_loss_l2 = l1_solver.fit(f=l1_model, X=np.nan, y=np.nan), \
+            l2_solver.fit(f=l2_model, X=np.nan, y=np.nan)
+        
+        print(f"The Lowest Loss Obtained for the L1-norm for eta={eta} is {lowest_loss_l1}.\n")
+        print(f"The Lowest Loss Obtained for the L2-norm for eta={eta} is {lowest_loss_l2}.\n")
+
+        plt.plot(l1_losses, label=f"L1-Norm recording with eta={eta}")
+        plt.plot(l2_losses, label=f"L2-Squared Norm recording with eta={eta}")
+        
+        if eta == .01:
+            plot_descent_path(module=L1,
+                              descent_path=np.array(l1_weights),
+                              title="L1-norm Gradient Descent Path").show()
+            plot_descent_path(module=L2,
+                              descent_path=np.array(l2_weights),
+                              title="L2-norm Gradient Descent Path").show()
+
+    plt.legend()
+    plt.show()
 
 
 def compare_exponential_decay_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
                                     eta: float = .1,
                                     gammas: Tuple[float] = (.9, .95, .99, 1)):
-    # Optimize the L1 objective using different decay-rate values of the exponentially decaying learning rate
+    # Optimize the L1 objective using different decay-rate values of 
+    # the exponentially decaying learning rate
     raise NotImplementedError()
 
     # Plot algorithm's convergence for the different values of gamma
@@ -133,8 +177,8 @@ def fit_logistic_regression():
     # Plotting convergence rate of logistic regression over SA heart disease data
     raise NotImplementedError()
 
-    # Fitting l1- and l2-regularized logistic regression models, using cross-validation to specify values
-    # of regularization parameter
+    # Fitting l1- and l2-regularized logistic regression models, 
+    # using cross-validation to specify values of regularization parameter
     raise NotImplementedError()
 
 